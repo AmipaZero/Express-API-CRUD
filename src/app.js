e62725b25ef15d4ac6cb9408f11fb8027e66432f
AMIPA UMADINIAH <nyaaaku5@gamil.com>
2025-07-13 08:50:22 +0700

    Initial commit - Express + Prisma API

---
+++ src/app.js
@@ -0,0 +1,19 @@
+import productRoutes from './routes/productRoutes.js';
+import express from 'express';
+import dotenv from 'dotenv';
+import { PrismaClient } from '@prisma/client';
+
+dotenv.config(); 
+const prisma = new PrismaClient();
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use('/api/products', productRoutes);
+app.get("/api", (req, res) => {
+  res.send("Hiiii");
+});
+
+app.listen(PORT, () => {
+  console.log("Express API running on port: " + PORT);
+});
---
+++ src/controllers/productController.js
@@ -0,0 +1,47 @@
+import * as productService from '../services/productService.js';
+
+export const getAll = async (req, res) => {
+  try {
+    const products = await productService.getAllProducts();
+    res.json(products);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+export const getOne = async (req, res) => {
+  try {
+    const product = await productService.getProductById(req.params.id);
+    if (!product) return res.status(404).json({ error: 'Product not found' });
+    res.json(product);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+export const create = async (req, res) => {
+  try {
+    const product = await productService.createProduct(req.body);
+    res.status(201).json(product);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+};
+
+export const update = async (req, res) => {
+  try {
+    const product = await productService.updateProduct(req.params.id, req.body);
+    res.json(product);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+};
+
+export const remove = async (req, res) => {
+  try {
+    await productService.deleteProduct(req.params.id);
+    res.json({ message: 'Product deleted' });
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+};
