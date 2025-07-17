d93ac750fb3288895efc1d58005bba7729a6fefa
AMIPA UMADINIAH <nyaaaku5@gamil.com>
2025-07-18 05:36:25 +0700

    Update app.js and productController.js

--- src/app.js
+++ src/app.js
@@ -9,10 +9,8 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use('/api/products', productRoutes);
-app.get("/api", (req, res) => {
-  res.send("Hiiii");
-});
+app.use('/api/produks', productRoutes);
+
 
 app.listen(PORT, () => {
   console.log("Express API running on port: " + PORT);
--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,6 +1,6 @@
 import * as productService from '../services/productService.js';
 
-export const getAll = async (req, res) => {
+ const getAll = async (req, res) => {
   try {
     const products = await productService.getAllProducts();
     res.json(products);
@@ -9,17 +9,17 @@ export const getAll = async (req, res) => {
   }
 };
 
-export const getOne = async (req, res) => {
+ const getOne = async (req, res) => {
   try {
     const product = await productService.getProductById(req.params.id);
-    if (!product) return res.status(404).json({ error: 'Product not found' });
+    if (!product) return res.status(404).json({ error: 'Produk tidak ditemukan!' });
     res.json(product);
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
 };
 
-export const create = async (req, res) => {
+ const create = async (req, res) => {
   try {
     const product = await productService.createProduct(req.body);
     res.status(201).json(product);
@@ -28,7 +28,7 @@ export const create = async (req, res) => {
   }
 };
 
-export const update = async (req, res) => {
+ const update = async (req, res) => {
   try {
     const product = await productService.updateProduct(req.params.id, req.body);
     res.json(product);
@@ -37,11 +37,19 @@ export const update = async (req, res) => {
   }
 };
 
-export const remove = async (req, res) => {
+ const remove = async (req, res) => {
   try {
     await productService.deleteProduct(req.params.id);
     res.json({ message: 'Product deleted' });
   } catch (err) {
     res.status(400).json({ error: err.message });
   }
+};
+
+export {
+getAll,
+getOne,
+create,
+update,
+remove
 };
