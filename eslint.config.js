22603a8753937f950d909b00af10a23de4501df9
AMIPA UMADINIAH <nyaaaku5@gamil.com>
2025-12-10 20:42:20 +0700

    add jenkinsfile

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,19 @@ import pluginReact from "eslint-plugin-react";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
-  pluginReact.configs.flat.recommended,
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node
+      }
+    },
+    rules: {
+      "no-unused-vars": "off"
+    }
+  },
+  pluginReact.configs.flat.recommended
 ]);
